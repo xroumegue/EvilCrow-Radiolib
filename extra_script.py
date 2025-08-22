c9b4e79d1418aabdfaa176a501c9f0501be48f9d
Xavier Roumegue <xroumegue@gmail.com>
2025-08-23 01:00:13 +0200

    Patch linker sections file
    Signed-off-by: Xavier Roumegue <xroumegue@gmail.com>

--- extra_script.py
+++ extra_script.py
@@ -1,8 +1,28 @@
 Import("env")
+from pathlib import Path
+from subprocess import run
+from shutil import copyfile
 
-env.Append(LINKFLAGS=[
-    "-L$PROJECT_DIR/etc/ld",
-    "-Tshell.ld",
-    "-Wl,-Map=$BUILD_DIR\\firmware.map"
-])
+build_dir=Path(env["PROJECT_BUILD_DIR"]).joinpath(env["PIOENV"])
+env.Replace(COMPILATIONDB_INCLUDE_TOOLCHAIN=True)
 
+linkFlags = str(env.Dictionary("LINKFLAGS")).replace("-T sections.ld", "-T sections-ecrf.ld")
+env.Replace(LINKFLAGS=linkFlags)
+sectionFile = None
+sectionLibPath =None
+for libpath in env.Dictionary("LIBPATH"):
+    sectionFile=Path(libpath).joinpath("sections.ld")
+    if (sectionFile.exists()):
+        print(sectionFile)
+        sectionLibPath = libpath 
+        break
+
+cp = run([
+    'patch',
+    '-i',
+    Path(env["PROJECT_DIR"]).joinpath("etc").joinpath("ld").joinpath("sections.ld.patch"),
+    '-o',
+    build_dir.joinpath("sections-ecrf.ld")
+    ],
+    cwd=sectionLibPath
+)
