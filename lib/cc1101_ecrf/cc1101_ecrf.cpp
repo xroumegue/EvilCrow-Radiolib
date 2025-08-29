315688c42857d1af3d782191c18858062a6746ff
Xavier Roumegue <xroumegue@gmail.com>
2025-08-30 00:46:07 +0200

    fixup! lib: cc1101_ecrf: Depends on eCC1101

--- lib/cc1101_ecrf/cc1101_ecrf.cpp
+++ lib/cc1101_ecrf/cc1101_ecrf.cpp
@@ -65,106 +65,6 @@ static size_t rxReceived = 0;
 static size_t rxLength;
 static eCC1101 *pCC1101 = NULL;
 
-static const uint32_t subghz_frequency_list[] = {
-    /* 300 - 348 */
-    300000000,
-    302757000,
-    303875000,
-    303900000,
-    304250000,
-    307000000,
-    307500000,
-    307800000,
-    309000000,
-    310000000,
-    312000000,
-    312100000,
-    312200000,
-    313000000,
-    313850000,
-    314000000,
-    314350000,
-    314980000,
-    315000000,
-    318000000,
-    330000000,
-    345000000,
-    348000000,
-    350000000,
-
-    /* 387 - 464 */
-    387000000,
-    390000000,
-    418000000,
-    430000000,
-    430500000,
-    431000000,
-    431500000,
-    433075000, /* LPD433 first */
-    433220000,
-    433420000,
-    433657070,
-    433889000,
-    433920000, /* LPD433 mid */
-    434075000,
-    434176948,
-    434190000,
-    434390000,
-    434420000,
-    434620000,
-    434775000, /* LPD433 last channels */
-    438900000,
-    440175000,
-    464000000,
-    467750000,
-
-    /* 779 - 928 */
-    779000000,
-    868350000,
-    868400000,
-    868800000,
-    868950000,
-    906400000,
-    915000000,
-    925000000,
-    928000000,
-};
-
-typedef struct {
-  uint32_t frequency_coarse;
-  int rssi_coarse;
-  uint32_t frequency_fine;
-  int rssi_fine;
-} FrequencyRSSI;
-
-const int rssi_threshold = -75;
-
-uint8_t cc1101_get_radio_state(eCC1101 *cc1101) {
-
-  int8_t radio_state =
-      cc1101->SPIgetRegValue(RADIOLIB_CC1101_REG_MARCSTATE, 4, 0);
-#if CC1101_DEBUG
-  char buf[64];
-  snprintf(buf, 64, "[CC1101] FSM: %#x", radio_state);
-  Serial.println(F(buf));
-#endif
-
-  return radio_state;
-}
-
-uint8_t cc1101_get_rxfifo_available(eCC1101 *cc1101) {
-  uint8_t bytesInFIFO =
-      cc1101->SPIgetRegValue(RADIOLIB_CC1101_REG_RXBYTES, 6, 0);
-
-#if CC1101_DEBUG
-  char buf[64];
-  snprintf(buf, 64, "[CC1101] RXFIFO AVAILABLE: %d", bytesInFIFO);
-  Serial.println(F(buf));
-#endif
-
-  return bytesInFIFO;
-}
-
 static eCC1101 *cc1101_init(int id) {
 
   if ((id < 0) || (id > 1)) {
@@ -174,27 +74,8 @@ static eCC1101 *cc1101_init(int id) {
 
   eCC1101 *cc1101 = &ecrf_radios[id];
   cc1101->begin();
-
-  cc1101_get_radio_state(cc1101);
+  cc1101->get_radio_state();
   return cc1101;
-}
-
-static int16_t cc1101_setInfiniteLengthMode(eCC1101 *cc1101)
-{
-    // infinite packet mode
-    int16_t state = cc1101->SPIsetRegValue(RADIOLIB_CC1101_REG_PKTCTRL0, RADIOLIB_CC1101_LENGTH_CONFIG_INFINITE, 1, 0);
-    RADIOLIB_ASSERT(state);
-    /* PKT MUST NOT BE 0 */
-    size_t len = 255;
-    state = cc1101->SPIsetRegValue(RADIOLIB_CC1101_REG_PKTLEN, len);
-    RADIOLIB_ASSERT(state);
-    // no longer in a direct mode
-    cc1101->directModeEnabled = false;
-    // update the cached values
-    cc1101->packetLength = len;
-    cc1101->packetLengthConfig = RADIOLIB_CC1101_LENGTH_CONFIG_INFINITE;
-
-    return state;
 }
 
 static TaskHandle_t xHandlingTask = NULL;
@@ -231,7 +112,7 @@ static BaseType_t cc1101_receiveStream(eCC1101 *cc1101, unsigned char *data, siz
       if ((ulNotifiedValue & RX_BIT) != 0) {
 #if 1
         uint8_t bytesInFIFO =
-            MIN(cc1101_get_rxfifo_available(cc1101), remaining);
+            MIN(cc1101->get_rxfifo_available(), remaining);
         cc1101->SPIreadRegisterBurst(RADIOLIB_CC1101_REG_FIFO, bytesInFIFO,
                                      data);
         data += bytesInFIFO;
@@ -254,24 +135,6 @@ static BaseType_t cc1101_receiveStream(eCC1101 *cc1101, unsigned char *data, siz
   return len - remaining;
 }
 
-struct s_cc1101_rf_rx_settings {
-    float freq;
-    float br;
-    float freqDev;
-    float rxBw;
-    uint8_t modulation;
-};
-
-static BaseType_t cc1101_set_rf(eCC1101 *cc1101, s_cc1101_rf_rx_settings *settings) {
-  cc1101->setFrequency(settings->freq);
-  cc1101->setBitRate(settings->br);
-  cc1101->setFrequencyDeviation(settings->freqDev);
-  cc1101->setRxBandwidth(settings->rxBw);
-  cc1101->setOOK(settings->modulation == RADIOLIB_CC1101_MOD_FORMAT_ASK_OOK);
-
-  return pdFALSE;
-}
-
 static BaseType_t cc1101_receive(eCC1101 *cc1101, s_cc1101_rf_rx_settings *settings, unsigned char *data, size_t len) {
   xHandlingTask = xTaskGetCurrentTaskHandle();
 
@@ -282,11 +145,11 @@ static BaseType_t cc1101_receive(eCC1101 *cc1101, s_cc1101_rf_rx_settings *setti
   state = cc1101->SPIsetRegValue(RADIOLIB_CC1101_REG_PKTCTRL1,
                                  RADIOLIB_CC1101_APPEND_STATUS_OFF, 3, 3);
 
-  state = cc1101_set_rf(cc1101, settings);
+  state = cc1101->set_rf(settings);
   state = cc1101->disableAddressFiltering();
   cc1101->SPIsetRegValue(RADIOLIB_CC1101_REG_MCSM1, RADIOLIB_CC1101_RXOFF_RX, 3,
                          2);
-  cc1101_setInfiniteLengthMode(cc1101);
+  cc1101->setInfiniteLengthMode();
 
   size_t received = cc1101_receiveStream(cc1101, data, len);
   cc1101->clearPacketReceivedAction();
@@ -294,59 +157,6 @@ static BaseType_t cc1101_receive(eCC1101 *cc1101, s_cc1101_rf_rx_settings *setti
   cc1101->setPromiscuousMode(false, false);
 
   return received;
-}
-
-static BaseType_t cc1101_scan(eCC1101 *radio, FrequencyRSSI *frequency_rssi) {
-  int rssi;
-  *frequency_rssi = {.frequency_coarse = 0,
-                     .rssi_coarse = -100,
-                     .frequency_fine = 0,
-                     .rssi_fine = -100};
-
-  // First stage: coarse scan
-  radio->setRxBandwidth(
-      650); // 58, 68, 81, 102, 116, 135, 162, 203, 232, 270, 325, 406, 464,
-  // 541, 650 and 812 kHz    (81kHz seems to work best for me)
-  size_t array_size =
-      sizeof(subghz_frequency_list) / sizeof(subghz_frequency_list[0]);
-  for (size_t i = 0; i < array_size; i++) {
-    uint32_t frequency = subghz_frequency_list[i];
-    if (frequency != 467750000 && frequency != 464000000 &&
-        frequency != 390000000 && frequency != 312000000 &&
-        frequency != 312100000 && frequency != 312200000 &&
-        frequency != 440175000) {
-      radio->setFrequency((float)frequency / 1000000.0);
-      radio->receiveDirect();
-      delay(2);
-      rssi = radio->getRSSI();
-
-      if (frequency_rssi->rssi_coarse < rssi) {
-        frequency_rssi->rssi_coarse = rssi;
-        frequency_rssi->frequency_coarse = frequency;
-      }
-    }
-  }
-
-  // Second stage: fine scan
-  if (frequency_rssi->rssi_coarse > rssi_threshold) {
-    // for example -0.3 ... 433.92 ... +0.3 step 20KHz
-    radio->setRxBandwidth(58);
-    for (uint32_t i = frequency_rssi->frequency_coarse - 300000;
-         i < frequency_rssi->frequency_coarse + 300000; i += 20000) {
-      uint32_t frequency = i;
-      radio->setFrequency((float)frequency / 1000000.0);
-      radio->receiveDirect();
-      delay(2);
-      rssi = radio->getRSSI();
-
-      if (frequency_rssi->rssi_fine < rssi) {
-        frequency_rssi->rssi_fine = rssi;
-        frequency_rssi->frequency_fine = frequency;
-      }
-    }
-  }
-
-  return pdFALSE;
 }
 
 static BaseType_t cc1101_init_cmd(char *pcWriteBuffer, size_t xWriteBufferLen,
@@ -368,11 +178,11 @@ static BaseType_t cc1101_init_cmd(char *pcWriteBuffer, size_t xWriteBufferLen,
 
   return pdFALSE;
 }
-
 FREERTOS_SHELL_CMD_REGISTER("init", "init <radio id>", cc1101_init_cmd, 1);
 
 static BaseType_t cc1101_scan_cmd(char *pcWriteBuffer, size_t xWriteBufferLen,
                                   const char *pcCommandString) {
+  const int rssi_threshold = -75;
   int id;
   static int scan_loop = 0;
   size_t len = 0;
@@ -398,7 +208,7 @@ static BaseType_t cc1101_scan_cmd(char *pcWriteBuffer, size_t xWriteBufferLen,
 
   while (scan_loop-- > 0) {
     FrequencyRSSI rssi_scan;
-    cc1101_scan(pCC1101, &rssi_scan);
+    pCC1101->scan(&rssi_scan);
     if (rssi_scan.rssi_fine > rssi_threshold) {
       // Deliver results fine
       len = snprintf(pcWriteBuffer, xWriteBufferLen,
